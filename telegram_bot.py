96f79ed66d88c029c0a279e95350506529b2bc6b
E3Tmn <100991827+E3Tmn@users.noreply.github.com>
2023-09-27 11:19:07 +0300

    Делаем оплату.
    Не готово

--- telegram_bot.py
+++ telegram_bot.py
@@ -1,7 +1,9 @@
-from aiogram import Bot, types 
+from aiogram import Bot, types
 from aiogram.dispatcher import Dispatcher
 from aiogram.utils import executor
 from aiogram.types import InputFile, InputMedia
+
+from aiogram.types import Message, LabeledPrice, PreCheckoutQuery, ContentType
 import random
 from dotenv import load_dotenv
 import os
@@ -20,10 +22,49 @@ def main():
     new_recipe = InlineKeyboardButton('Новый рецепт', callback_data='new_recipe')
     welcome_buttons = InlineKeyboardMarkup(resize_keyboard=True).add(subscribe, new_recipe)
 
-    @dp.callback_query_handler(lambda c: c.data == 'subscribe')# Отзыв на первую кнопку
-    async def process_callback_subscribe(callback_query: types.CallbackQuery):
-        await bot.answer_callback_query(callback_query.id)
-        await bot.send_message(callback_query.from_user.id, 'Нажата первая кнопка!')
+    @dp.pre_checkout_query_handler(lambda query: True)# Это мне для оплаты
+    async def pre_checkout_query(pre_checkout_query: types.PreCheckoutQuery):
+        await bot.answer_pre_checkout_query(pre_checkout_query.id, ok=False, error_message='Что-то не так')
+
+
+    @dp.callback_query_handler(lambda c: c.data == 'subscribe')# Первая кнопка. Покупка
+    async def process_callback_subscribe(call: types.CallbackQuery):
+        await bot.send_invoice(
+        chat_id=call.message.chat.id,
+        title='Покупа через Телеграм бот',
+        description='Проверка работы',
+        payload='Что эТО??',
+        provider_token='381764678:TEST:67486',
+        currency='rub',
+        prices=[
+            LabeledPrice(
+                label='Подписка на канал',
+                amount=35000 
+            )
+        ],
+        max_tip_amount=500,
+        start_parameter='Pin_kod',
+        provider_data=None,
+        # photo_url='picture_for_pay.jpg',
+        photo_size=100,
+        need_name=True,
+        need_email=True,
+        need_phone_number=True,
+        need_shipping_address=False,
+        send_phone_number_to_provider=False,
+        send_email_to_provider=False,
+        is_flexible=False,
+        disable_notification=False,
+        protect_content=False,
+        reply_to_message_id=None,
+        allow_sending_without_reply=True,
+        reply_markup=None
+    )
+
+    @dp.message_handler(content_types=ContentType.SUCCESSFUL_PAYMENT) # Это моё после оплаты
+    async def successfull_payment(message: Message):
+        msg = f'Спасибо за оплату {message.successful_payment.total_amount // 100} {message.successful_payment.currency}.'
+        await message.answer(msg)
 
     @dp.callback_query_handler(lambda c: c.data == 'new_recipe')# Отзыв на вторую кнопку. После 3 раз крашится. Надо исправлять
     async def process_callback_new_recipe(call: types.CallbackQuery):
