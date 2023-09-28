ca5e04de2240f26dab73f8225c2f3e1a93cd9c88
Elizaveta Shvedova <elizabethsweden@icloud.com>
2023-09-28 10:47:00 +0300

    Совместила кнопки меню бота и блок с оплатой

--- telegram_bot.py
+++ telegram_bot.py
@@ -28,12 +28,36 @@ subscribe = InlineKeyboardButton('Оформить подписку', callback_d
 new_recipe = InlineKeyboardButton('Новый рецепт', callback_data='new_recipe')
 welcome_buttons = InlineKeyboardMarkup(resize_keyboard=True).add(subscribe, new_recipe)
 
+a=InlineKeyboardButton('до 1500', callback_data='racion_kb')
+b=InlineKeyboardButton('до 2500', callback_data='racion_kb')
+с=InlineKeyboardButton('больше 2500', callback_data='racion_kb') 
+calories_kb = InlineKeyboardMarkup(resize_keyboard=True).add(a,b,с)
+
+vegan_dishes = InlineKeyboardButton('Вегитарианское меню', callback_data='dishes_kb')
+meat_dishes = InlineKeyboardButton('Сытно и полезно', callback_data='dishes_kb')
+joy_dishes = InlineKeyboardButton('Быстро и вкусно', callback_data='dishes_kb') 
+racion_kb = InlineKeyboardMarkup(resize_keyboard=True).add(vegan_dishes, meat_dishes, joy_dishes)
+
+one_dish = InlineKeyboardButton('Белая', callback_data='period')
+two_dishes = InlineKeyboardButton('Синяя', callback_data='period')
+three_dishes = InlineKeyboardButton('Красная', callback_data='period') 
+dishes_kb = InlineKeyboardMarkup(resize_keyboard=True).add(one_dish, two_dishes, three_dishes)
+
+one_month = InlineKeyboardButton('1 мес', callback_data='payment') #колбэк на кнопку с оплатой
+three_monthes = InlineKeyboardButton('3 мес', callback_data='payment')
+six_monthes = InlineKeyboardButton('6 мес', callback_data='payment') 
+period = InlineKeyboardMarkup(resize_keyboard=True).add(one_month, three_monthes, six_monthes)
+
+
+
+
 # Первые сообщения для клиента--------
 @dp.message_handler(commands=['start']) # Вывод сообщений после /start
 async def process_start_command(message: types.Message):
     await message.reply("Добро пожаловать в супер-пупер бот. Мы подберем Вам рецепт")
     await bot.send_photo(message.from_user.id, photo, caption='Рецепт для вас', reply_markup=welcome_buttons)
     click_counter['new_recipe'] = 2
+
 
 @dp.callback_query_handler(lambda c: c.data == 'new_recipe')# Отзыв на вторую кнопку. После 3 раз крашится. Надо исправлять
 async def process_callback_new_recipe(call: types.CallbackQuery):
@@ -46,6 +70,28 @@ async def process_callback_new_recipe(call: types.CallbackQuery):
         await call.message.edit_media(file,reply_markup=welcome_buttons)
     click_counter['new_recipe'] -= 1
 
+
+@dp.callback_query_handler(lambda c: c.data == 'subscribe')
+async def choose_calories(callback_query: types.CallbackQuery):
+    await bot.send_message(callback_query.from_user.id , 'Выберете желаемую калорийность', reply_markup=calories_kb)
+
+    
+@dp.callback_query_handler(lambda c: c.data == 'racion_kb')
+async def choose_racion(callback_query: types.CallbackQuery):
+    await bot.send_message(callback_query.from_user.id , 'Выберете рацион', reply_markup=racion_kb)
+
+
+@dp.callback_query_handler(lambda c: c.data == 'dishes_kb')
+async def choose_amount(callback_query: types.CallbackQuery):
+    await bot.send_message(callback_query.from_user.id , 'Мы предлагаем вам 3 варианта подписки и описываем каждую...', reply_markup=dishes_kb)
+
+
+@dp.callback_query_handler(lambda c: c.data == 'period')
+async def choose_period(callback_query: types.CallbackQuery):
+    await bot.send_message(callback_query.from_user.id , 'Выберете срок и описываем 1месяц за 150р и тд', reply_markup=period)
+
+
+
 #---------------------
 
 # ВСЕ ПО ОПЛАТЕ--------
@@ -54,7 +100,7 @@ async def pre_checkout_query(pre_checkout_query: types.PreCheckoutQuery):
     await bot.answer_pre_checkout_query(pre_checkout_query.id, ok=True)
 
 
-@dp.callback_query_handler(lambda c: c.data == 'subscribe')# Первая кнопка. Покупка
+@dp.callback_query_handler(lambda c: c.data == 'payment')# Первая кнопка. Покупка
 async def process_callback_subscribe(call: types.CallbackQuery):
     await bot.send_invoice(
     chat_id=call.message.chat.id,
