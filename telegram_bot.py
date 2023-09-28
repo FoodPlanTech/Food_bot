aaa31521a2cffed740070aaeff03caf4a109b00f
E3Tmn <nick1tapinyagin@yandex.ru>
2023-09-28 09:16:23 +0300

    Сделал оплату и новые рецепты

--- telegram_bot.py
+++ telegram_bot.py
@@ -11,77 +11,90 @@ import os
 from aiogram.types import ReplyKeyboardRemove, \
     ReplyKeyboardMarkup, KeyboardButton, \
     InlineKeyboardMarkup, InlineKeyboardButton
+# import requests
 
-def main():
-    load_dotenv() 
-    bot = Bot(token=os.environ['TELEGRAM_TOKEN'])
-    dp = Dispatcher(bot)   
-    foods = ['eda.jpg', 'eda2.jpg','eda3.jpg']
-    photo = InputFile(foods[0])
-    subscribe = InlineKeyboardButton('Оформить подписку', callback_data='subscribe')
-    new_recipe = InlineKeyboardButton('Новый рецепт', callback_data='new_recipe')
-    welcome_buttons = InlineKeyboardMarkup(resize_keyboard=True).add(subscribe, new_recipe)
+# resp = requests.get('http://v1131340.hosted-by-vdsina.ru:5555/api/v1/recipes/')
+# print(resp.json())
 
-    @dp.pre_checkout_query_handler(lambda query: True)# Это мне для оплаты
-    async def pre_checkout_query(pre_checkout_query: types.PreCheckoutQuery):
-        await bot.answer_pre_checkout_query(pre_checkout_query.id, ok=False, error_message='Что-то не так')
+click_counter = {}
 
+load_dotenv() 
+bot = Bot(token=os.environ['TELEGRAM_TOKEN'])
+dp = Dispatcher(bot)   
+foods_photo = ['eda.jpg', 'eda2.jpg','eda3.jpg']
+foods_recipe = ['ПОКУШАЕМ?','ВКУСНО ПОКУШАЕМ?','ОЧЕНЬ ВКУСНО ПОКУШАЕМ?']
+photo = InputFile(foods_photo[0])
+subscribe = InlineKeyboardButton('Оформить подписку', callback_data='subscribe')
+new_recipe = InlineKeyboardButton('Новый рецепт', callback_data='new_recipe')
+welcome_buttons = InlineKeyboardMarkup(resize_keyboard=True).add(subscribe, new_recipe)
 
-    @dp.callback_query_handler(lambda c: c.data == 'subscribe')# Первая кнопка. Покупка
-    async def process_callback_subscribe(call: types.CallbackQuery):
-        await bot.send_invoice(
-        chat_id=call.message.chat.id,
-        title='Покупа через Телеграм бот',
-        description='Проверка работы',
-        payload='Что эТО??',
-        provider_token='381764678:TEST:67486',
-        currency='rub',
-        prices=[
-            LabeledPrice(
-                label='Подписка на канал',
-                amount=35000 
-            )
-        ],
-        max_tip_amount=500,
-        start_parameter='Pin_kod',
-        provider_data=None,
-        # photo_url='picture_for_pay.jpg',
-        photo_size=100,
-        need_name=True,
-        need_email=True,
-        need_phone_number=True,
-        need_shipping_address=False,
-        send_phone_number_to_provider=False,
-        send_email_to_provider=False,
-        is_flexible=False,
-        disable_notification=False,
-        protect_content=False,
-        reply_to_message_id=None,
-        allow_sending_without_reply=True,
-        reply_markup=None
-    )
+# Первые сообщения для клиента--------
+@dp.message_handler(commands=['start']) # Вывод сообщений после /start
+async def process_start_command(message: types.Message):
+    await message.reply("Добро пожаловать в супер-пупер бот. Мы подберем Вам рецепт")
+    await bot.send_photo(message.from_user.id, photo, caption='Рецепт для вас', reply_markup=welcome_buttons)
+    click_counter['new_recipe'] = 2
 
-    @dp.message_handler(content_types=ContentType.SUCCESSFUL_PAYMENT) # Это моё после оплаты
-    async def successfull_payment(message: Message):
-        msg = f'Спасибо за оплату {message.successful_payment.total_amount // 100} {message.successful_payment.currency}.'
-        await message.answer(msg)
-
-    @dp.callback_query_handler(lambda c: c.data == 'new_recipe')# Отзыв на вторую кнопку. После 3 раз крашится. Надо исправлять
-    async def process_callback_new_recipe(call: types.CallbackQuery):
-        subscribe = InlineKeyboardButton('Оформить подписку', callback_data='subscribe')# 30,31,32 Строки возможно лишние. Надо тестить
-        new_recipe = InlineKeyboardButton('Новый рецепт', callback_data='new_recipe')
-        welcome_buttons = InlineKeyboardMarkup(resize_keyboard=True).add(subscribe, new_recipe)
-        file_path = InputFile(random.sample(foods, 1)[0])
-        file = InputMedia(media=file_path, caption="Updated caption :)")
+@dp.callback_query_handler(lambda c: c.data == 'new_recipe')# Отзыв на вторую кнопку. После 3 раз крашится. Надо исправлять
+async def process_callback_new_recipe(call: types.CallbackQuery):
+    file_path = InputFile(foods_photo[click_counter['new_recipe']])
+    recipe = foods_recipe[click_counter['new_recipe']]
+    file = InputMedia(media=file_path, caption=recipe)
+    if click_counter['new_recipe'] == 1:
+        await call.message.edit_media(file,reply_markup=InlineKeyboardMarkup(resize_keyboard=True).add(subscribe))
+    else:
         await call.message.edit_media(file,reply_markup=welcome_buttons)
+    click_counter['new_recipe'] -= 1
 
-    @dp.message_handler(commands=['start']) # Вывод сообщений после /start
-    async def process_start_command(message: types.Message):
-        await message.reply("Добро пожаловать в супер-пупер бот. Мы подберем Вам рецепт")
-        await bot.send_photo(message.from_user.id, photo, caption='Рецепт для вас', reply_markup=welcome_buttons)
+#---------------------
 
-    executor.start_polling(dp)
+# ВСЕ ПО ОПЛАТЕ--------
+@dp.pre_checkout_query_handler(lambda query: True)# Процесс оплаты
+async def pre_checkout_query(pre_checkout_query: types.PreCheckoutQuery):
+    await bot.answer_pre_checkout_query(pre_checkout_query.id, ok=True)
+
+
+@dp.callback_query_handler(lambda c: c.data == 'subscribe')# Первая кнопка. Покупка
+async def process_callback_subscribe(call: types.CallbackQuery):
+    await bot.send_invoice(
+    chat_id=call.message.chat.id,
+    title='Подписка',
+    description='Оформляем подписку на канал',
+    payload='Покупа через Телеграм бот',
+    provider_token= os.environ['PROVIDER_TOKEN'],
+    currency='rub',
+    prices=[
+        LabeledPrice(
+            label='Подписка на канал',
+            amount=10000 
+        )
+    ],
+    max_tip_amount=500,
+    start_parameter="one_month_sub",
+    need_name=True,
+    need_email=True,
+    need_phone_number=True,
+    need_shipping_address=False,
+    send_phone_number_to_provider=False,
+    send_email_to_provider=False,
+    is_flexible=False,
+    disable_notification=False,
+    protect_content=False,
+    reply_to_message_id=None,
+    allow_sending_without_reply=True,
+    reply_markup=None
+)
+
+@dp.message_handler(content_types=ContentType.SUCCESSFUL_PAYMENT) # Процесс после оплаты
+async def successfull_payment(message: Message):
+    purchase_message = message.successful_payment.to_python()
+    for key, val in purchase_message.items():
+        print(f'{key} = {val}')
+    msg = f'Спасибо за оплату {message.successful_payment.total_amount // 100} {message.successful_payment.currency}.'
+    await message.answer(msg)
+# Оплата кончилась------
+    
 
 if __name__ == '__main__':  
-    main()
+    executor.start_polling(dp)
     
