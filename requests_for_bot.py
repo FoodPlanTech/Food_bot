5865040cba189b6a1bdd528243880539f9221856
Elizaveta Shvedova <elizabethsweden@icloud.com>
2023-09-28 17:27:21 +0300

    Add file

---
+++ requests_for_bot.py
@@ -0,0 +1,15 @@
+import requests
+import collections  
+# import pprint
+
+def get_recipes():
+    response = requests.get('http://v1131340.hosted-by-vdsina.ru:5555/api/v1/teasers')
+    response.raise_for_status()
+    cards_for_recipe = []
+    for recipe in response.json():
+        cards_for_recipe.append(recipe)
+    
+    return cards_for_recipe
+#     pprint.pprint(cards_for_recipe[0]['title'])
+
+# get_recipes()
