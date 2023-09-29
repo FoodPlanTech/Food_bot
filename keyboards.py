9fcce7535d48b86f3516322108b72825d961e208
Elizaveta Shvedova <elizabethsweden@icloud.com>
2023-09-29 23:34:13 +0300

    Удалила дубль InlineKeyboardMarkup

--- keyboards.py
+++ keyboards.py
@@ -88,15 +88,9 @@ select_dishes = InlineKeyboardMarkup(inline_keyboard=[
 preferences = get_preferences()
 preferences_list =[]
 for pref in preferences:
-        preferences_list.append([InlineKeyboardButton(text = pref['title'], callback_data = pref['id'])])
+        preferences_list.append([InlineKeyboardButton(text = pref['title'], callback_data = 'dishes_kb')])
         racion_kb = InlineKeyboardMarkup(inline_keyboard = preferences_list)
-        print(racion_kb)
 select_racion = racion_kb
-
-one_dish = InlineKeyboardButton('Белая', callback_data='period')
-two_dishes = InlineKeyboardButton('Синяя', callback_data='period')
-three_dishes = InlineKeyboardButton('Красная', callback_data='period') 
-dishes_kb = InlineKeyboardMarkup(resize_keyboard=True).add(one_dish, two_dishes, three_dishes)
 
 
 select_period = InlineKeyboardMarkup(inline_keyboard=[
