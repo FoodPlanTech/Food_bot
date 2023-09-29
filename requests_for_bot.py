5ee6f14c52690f0f6a27ed4da55b7e636efbbdf7
Elizaveta Shvedova <elizabethsweden@icloud.com>
2023-09-29 20:59:50 +0300

    Написала post запрос c выбранным preference(рационом) в БД

--- requests_for_bot.py
+++ requests_for_bot.py
@@ -11,5 +11,30 @@ def get_recipes():
     
     return cards_for_recipe
 #     pprint.pprint(cards_for_recipe[0]['title'])
-
 # get_recipes()
+
+
+def send_id(id):
+    url = 'http://v1131340.hosted-by-vdsina.ru:5555/api/v1/tg-accounts/'
+    payload = {
+    'telegram_id': id
+        }
+    response_post = requests.post(url, data=payload)
+    response_post.raise_for_status()
+
+
+def get_preferences():
+    response = requests.get('http://v1131340.hosted-by-vdsina.ru:5555/api/v1/preferences')
+    response.raise_for_status()
+    preferences = response.json()
+    return preferences
+
+#def post_preference():
+#    url = 'http://v1131340.hosted-by-vdsina.ru:5555/api/v1/foodplans/'   
+#    payload = {
+#    'telegram_id': id
+#    'preference_id' : callback_query.data
+#        }
+#    response_post = requests.post(url, data=payload)
+#    response_post.raise_for_status()
+
