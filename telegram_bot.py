fe62716618c4191068b99905bfa123418405b483
Elizaveta Shvedova <elizabethsweden@icloud.com>
2023-09-30 21:50:48 +0300

    Update telegram_bot.py

--- telegram_bot.py
+++ telegram_bot.py
@@ -8,7 +8,7 @@ from pay import process_callback_subscribe, pre_checkout_query,\
       successfull_payment
 from basics import choose_calories, process_start_command,\
     process_callback_new_recipe, choose_amount, choose_period, choose_racion,choose_recipe
-from keyboards import racion_id_buttons
+from keyboards import racion_id_buttons, subscribtion_id_buttons
 
 load_dotenv()
 bot = Bot(token=os.environ['TELEGRAM_TOKEN'])
@@ -18,7 +18,7 @@ dp.register_callback_query_handler(process_callback_new_recipe, lambda c: c.data
 dp.register_callback_query_handler(choose_calories, lambda c: c.data == 'subscribe')
 dp.register_callback_query_handler(choose_racion, lambda c: c.data == 'racion_kb')
 dp.register_callback_query_handler(choose_amount, lambda c: c.data in racion_id_buttons)
-dp.register_callback_query_handler(choose_period, lambda c: c.data == 'period')
+dp.register_callback_query_handler(choose_period, lambda c: c.data == 'period')#c.data in subscribtion_id_buttons
 dp.register_callback_query_handler(process_callback_subscribe, lambda c: c.data == 'payment')
 dp.register_pre_checkout_query_handler(pre_checkout_query, lambda query: True)
 dp.register_message_handler(successfull_payment, content_types=ContentType.SUCCESSFUL_PAYMENT)
